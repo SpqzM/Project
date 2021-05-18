1d31c9b16748327ace07dcddf6e3af25e38b84c9
SpqzM <nico.davy@gmail.com>
2021-05-18 13:17:11 +0000

    forcer le https

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -9,7 +9,7 @@ class QuestionController extends Controller
 {
     public function index()
     {
-        $questions = Question::with('user')->latest()->simplePaginate(10);
+        $questions = Question::with('user')->latest()->Paginate(10);
         
         return view('questions.index', [
             'questions' => $questions    
